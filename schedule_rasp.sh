fb31f7a71c7375f1ceae73fbba290e2b20948e2f
staylr@gmail.com <ubuntu@ip-172-31-33-187.us-west-2.compute.internal>
2014-08-17 09:04:16 +0000

    Fix rasp_forecash.sh permissions.

--- schedule_rasp.sh
+++ schedule_rasp.sh
@@ -152,6 +152,8 @@ export BASEDIR=/home/admin/DRJACK
 s3cmd -c /home/ubuntu/.s3cfg sync "s3://$config_s3bucket/scripts/rasp_forecast.sh" "/home/admin/rasp_forecast.sh"
 s3cmd  -c /home/ubuntu/.s3cfg sync "s3://$config_s3bucket/config/rasp.run.parameters.*" "\$BASEDIR/RASP/RUN/"
 s3cmd  -c /home/ubuntu/.s3cfg sync "s3://$config_s3bucket/config/rasp.ncl.region.data" "\$BASEDIR/WRF/NCL/rasp.ncl.region.data"
+chown admin /home/admin/rasp_forecast.sh
+chmod +x /home/admin/rasp_forecast.sh
 
 cd /home/admin
 nohup sudo -n -i -u admin ./rasp_forecast.sh --s3bucket "s3://$s3bucket" --archive --shutdown $regions >& /var/log/rasp.log &
